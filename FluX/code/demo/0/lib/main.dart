83bb62b6e489e1a427a7acbadae7cd4c34484737
邓江洲 <dengjiangzhouReal@gmail.com>
2022-01-23 23:58:51 +0800

    start

---
+++ FluX/code/demo/0/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+
+
+// 相当于  UIKit
+import 'package:flutter/material.dart';
+
+
+
+
+
+void main(){
+
+
+  // 返回一个，根的 View
+
+
+
+
+  runApp(
+      Center(
+        child: Text(
+          "666 dng",
+          textDirection: TextDirection.ltr,
+        ),
+      )
+
+    );
+
+    //  child:
+    //  类似 subview
+
+
+   // Text
+   // 类似 UILabel
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
